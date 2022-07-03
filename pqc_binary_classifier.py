1951174f4bdfc193aee22100a5d40f187b66892c
ogatensor <90874464+ogatensor@users.noreply.github.com>
2022-07-03 14:27:01 +0100

    Update pqc_binary_classifier.py
    add confusion matrix, f1 scores, and other evaluation metric dependencies. setup Aer backend.

--- pqc_binary_classifier.py
+++ pqc_binary_classifier.py
@@ -1,6 +1,11 @@
 from qiskit import execute, Aer, QuantumCircuit
 from math import sqrt
 from sklearn.metrics import recall_score, precision_score, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score
+import numpy as np
+import random
+
+random.seed(a=None, version=2)
 
 def pqc_classify(backend, passenger_state):
     """backend -- a qiskit backend to run the quantum circuit at
@@ -23,3 +28,58 @@ def pqc_classify(backend, passenger_state):
     
     # get the bit 0 or 1
     return int(list(map(lambda item: item[0], counts.items()))[0])
+
+with open('train.npy', 'rb') as f:
+    train_input = np.load(f)
+    train_labels = np.load(f)
+
+with open('test.npy', 'rb') as f:
+    test_input = np.load(f)
+    test_labels = np.load(f)
+
+# REDEFINE OR IMPORT THE FUNCTIONS OF CHAPTER 2
+def run(f_classify, x):
+    return list(map(f_classify, x))
+
+def specificity(matrix):
+    return matrix[0][0]/(matrix[0][0]+matrix[0][1]) if (matrix[0][0]+matrix[0][1] > 0) else 0
+
+def npv(matrix):
+    return matrix[0][0]/(matrix[0][0]+matrix[1][0]) if (matrix[0][0]+matrix[1][0] > 0) else 0
+
+def classifier_report(name, run, classify, input, labels):
+    cr_predictions = run(classify, input)
+    cr_cm = confusion_matrix(labels, cr_predictions)
+
+    cr_precision = precision_score(labels, cr_predictions)
+    cr_recall = recall_score(labels, cr_predictions)
+    cr_specificity = specificity(cr_cm)
+    cr_npv = npv(cr_cm)
+    cr_level = 0.25*(cr_precision + cr_recall + cr_specificity + cr_npv)
+
+    print('The precision score of the {} classifier is {:.2f}'
+        .format(name, cr_precision))
+    print('The recall score of the {} classifier is {:.2f}'
+        .format(name, cr_recall))
+    print('The specificity score of the {} classifier is {:.2f}'
+        .format(name, cr_specificity))
+    print('The npv score of the {} classifier is {:.2f}'
+        .format(name, cr_npv))
+    print('The information level is: {:.2f}'
+        .format(cr_level))
+
+#CAPTION A reusable function to unmask the hypocrite classifier
+
+# The scores of the random quantum classifier
+# Tell Qiskit how to simulate our circuit
+backend = Aer.get_backend('statevector_simulator') 
+
+# Specify the quantum state that results in either 0 or 1
+initial_state = [1/sqrt(2), 1/sqrt(2)] 
+
+classifier_report("Random PQC", 
+    run,
+    lambda passenger: pqc_classify(backend, initial_state),
+    train_input,
+    train_labels) 
+
